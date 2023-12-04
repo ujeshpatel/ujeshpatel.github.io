c7c09fd050ed1e552e5bdfdb815aaa7c1ed1fce4
Ujesh Patel <erujeshpatel@gmail.com>
2023-12-04 12:46:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ window.onload = function() {
     var contactform = document.getElementById('contact-form');
     contactform.setAttribute('action', "https://formspree.io/f/mayzvnoo");
 }
+
+let scrollToBottom = document.querySelector("#scroll-to-bottom")
+let pageBottom = document.querySelector("#page-bottom")
+
+scrollToBottom.addEventListener("click", function() {
+  pageBottom.scrollIntoView()
+})
